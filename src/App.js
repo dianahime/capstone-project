277bb5a7980448a10263360effbca60f8a39534e
dianahime <dajana.pavlovic26@gmail.com>
2020-07-09 15:02:15 +0200

    Add components and functionality to app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { useState } from 'react'
-import Form from './Components/Form'
-import ProductsList from './Components/ProductsList'
+import Form from './components/Form'
+import ProductsList from './components/ProductsList'
+import Drawer from './components/Drawer'
+import styled from 'styled-components'
 
 function App() {
   const [products, setProducts] = useState([])
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
   return (
-    <>
-      <Form onFormSubmit={handleProducts} />
-      <ProductsList products={products} />
-    </>
+    <ContainerStyled>
+      <ProductsList isBlurred={isDrawerOpen} products={products} />
+      <Drawer isOpen={isDrawerOpen} setIsOpen={setIsDrawerOpen}>
+        <Form onFormSubmit={handleProducts} />
+      </Drawer>
+    </ContainerStyled>
   )
   function handleProducts(product) {
+    const maxId = Math.max(...products.map((product) => product.id))
+    product.id = maxId + 1
     setProducts([...products, product])
+    setIsDrawerOpen(false)
   }
 }
 
 export default App
+
+const ContainerStyled = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
