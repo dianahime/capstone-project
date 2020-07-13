b95b39f6abac0727dc9fef131192bfa219ca9003
dianahime <dajana.pavlovic26@gmail.com>
2020-07-13 12:38:10 +0200

    Add component tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('<App />', () => {
+  it('renders <App /> component correctly', () => {
+    const { getByText } = render(<App />)
+    const linkElement = getByText('Click on the plus icon to add a product.')
+
+    expect(linkElement).toBeInTheDocument()
+  })
+})
