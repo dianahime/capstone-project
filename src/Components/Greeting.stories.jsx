cfeaf475257068a8e932e610fb42094c6ea40b97
dianahime <34010932+dianahime@users.noreply.github.com>
2020-07-14 21:06:03 +0200

    Fix case problem (#12)
    * Delete Button.jsx
    
    * Delete Button.stories.jsx
    
    * Delete Button.test.jsx
    
    * Delete Drawer.jsx
    
    * Delete Drawer.stories.jsx
    
    * Delete Drawer.test.jsx
    
    * Delete DrawerToggle.jsx
    
    * Delete DrawerToggle.stories.jsx
    
    * Delete DrawerToggle.test.jsx
    
    * Delete Form.jsx
    
    * Delete Form.stories.jsx
    
    * Delete Form.test.jsx
    
    * Delete Greeting.jsx
    
    * Delete Greeting.stories.jsx
    
    * Delete Greeting.test.jsx
    
    * Delete InfoPopover.jsx
    
    * Delete InfoPopover.stories.jsx
    
    * Delete InfoPopover.test.jsx
    
    * Delete ProductsList.jsx
    
    * Delete ProductsList.stories.jsx
    
    * Delete ProductsList.test.jsx
    
    * Delete ProductsListItem.jsx
    
    * Delete ProductsListItem.stories.jsx
    
    * Delete ProductsListItem.test.jsx

--- src/Components/Greeting.stories.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Greeting from './Greeting'
-
-export default {
-  title: 'Greeting',
-  component: Greeting,
-}
-
-export const GreetingDefault = () => <Greeting />
