3a3f03c8a10b7700d2b45a042108cc92b469e592
dianahime <dajana.pavlovic26@gmail.com>
2020-07-17 10:40:35 +0200

    adjust cypress test for from input

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -1,11 +1,11 @@
 describe('Input form', () => {
   beforeEach(() => {
     cy.visit('/')
-    cy.get('.sc-Axmtr').click()
+    cy.get('.sc-AxgMl').click()
   })
 
   it('opens form', () => {
-    cy.get('.sc-Axmtr').click()
+    cy.get('.sc-AxgMl').click()
   })
 
   it('accepts input', () => {
@@ -62,19 +62,37 @@ describe('Input form', () => {
       cy.get('#date').type(itemOpeningDate)
       cy.get('#month').type(itemMonth).type('{enter}')
 
-      cy.get('.sc-Axmtr').click()
+      cy.get('.sc-AxgMl').click()
       cy.get('#name').type('Nivea night face cream long product name')
       cy.get('#date').type(itemOpeningDate)
       cy.get('#month').type(itemMonth).type('{enter}')
 
-      cy.get('.sc-Axmtr').click()
+      cy.get('.sc-AxgMl').click()
       cy.get('#name').type('Hairspray')
       cy.get('#date').type(itemOpeningDate)
-      cy.get('#month').type(itemMonth)
-      cy.get('.size').should('contain', 'Size: 50ml')
-      cy.get('.price').should('contain', '3,55$').type('{enter}')
+      cy.get('#month').type(itemMonth).type('{enter}')
 
       cy.get('li').should('have.length', 3)
+    })
+
+    it('has an info popover', () => {
+      cy.get('.fa').click()
+      cy.get('.content').should(
+        'contain',
+        'The time in months when the product will remain in good condition after you have used the product for the first time. A symbol of an open cream jar is usually used and the time in months can be inside the symbol or alongside it.'
+      )
+    })
+
+    it('saves products to localStorage', () => {
+      cy.get('#name').type(itemText)
+      cy.get('#date').type(itemOpeningDate)
+      cy.get('#month').type(itemMonth).type('{enter}')
+
+      cy.visit('/')
+
+      cy.get('li').should('have.length', 1).and('contain', itemText)
+      cy.get('.opening-date').should('contain', 'Opened: 27.05.2020')
+      cy.get('.expiring-date').should('contain', 'Expires: 27.11.2020')
     })
   })
 })
