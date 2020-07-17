92dc181f0e55cdc72809f4928b1f8d219025540a
dianahime <dajana.pavlovic26@gmail.com>
2020-07-17 11:13:49 +0200

    Add productDetails component

---
+++ src/components/ProductDetails.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import styled from 'styled-components'
+import dayjs from 'dayjs'
+import { useSelector } from 'react-redux'
+
+export default function ProductDetails() {
+  const product = useSelector((state) => state.products.selected)
+
+  const parsedDate = dayjs(product.date)
+
+  return (
+    <ProductStyled>
+      <h1 className="name">{product.name}</h1>
+      <p className="opening-date">Opened: {parsedDate.format('DD.MM.YYYY')}</p>
+      <p className="expiring-date">
+        Expires: {parsedDate.add(product.month, 'M').format('DD.MM.YYYY')}
+      </p>
+      {product.size && <p className="size">Size: {product.size}</p>}
+      {product.price && <p className="price">Price: {product.price}</p>}
+    </ProductStyled>
+  )
+}
+
+const ProductStyled = styled.section`
+  padding: 0 20px;
+
+  p {
+    font-size: 1.2rem;
+    margin: 10px 0;
+  }
+
+  .expiring-date {
+    color: var(--secondary);
+    margin-bottom: 20px;
+  }
+  .name {
+    font-size: 1.8rem;
+    width: 300px;
+  }
+`
