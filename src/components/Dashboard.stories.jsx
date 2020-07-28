68952e43570ba325bd0e5991b96fc49f0a0954dd
dianahime <dajana.pavlovic26@gmail.com>
2020-07-28 16:44:44 +0200

    Add dashboard storybook and routing for storybook

---
+++ src/components/Dashboard.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Dashboard from './Dashboard'
+
+export default {
+  title: 'Dashboard',
+  component: Dashboard,
+}
+
+export const DashboardDefault = () => <Dashboard />
