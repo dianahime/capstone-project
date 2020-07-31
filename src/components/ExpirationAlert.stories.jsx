c7400bcca2ff0f04939677e58be79f4e63e111c3
dianahime <dajana.pavlovic26@gmail.com>
2020-07-31 12:06:49 +0200

    Add stroybook for Archive components

---
+++ src/components/ExpirationAlert.stories.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ExpirationAlert from './ExpirationAlert'
+
+export default {
+  title: 'ExpirationAlert',
+  component: ExpirationAlert,
+}
+const product = {
+  name: "test",
+  date: "2020-05-01",
+  month: 1
+}
+export const ExpirationAlertDefault = () => <ExpirationAlert product={product}/>
---
+++ src/components/ProductsNavigation.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ProductsNavigation from './ProductsNavigation'
+
+export default {
+  title: 'ProductsNavigation',
+  component: ProductsNavigation,
+}
+
+export const ProductsNavigationDefault = () => <ProductsNavigation />
