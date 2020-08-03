c6f0dfabe663cb495566e41b4fc6caca4c605d79
dianahime <dajana.pavlovic26@gmail.com>
2020-08-03 10:42:41 +0200

    Add sorting to archive

--- src/components/ArchivedProductsList.jsx
+++ src/components/ArchivedProductsList.jsx
@@ -1,23 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import { useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 import { selectors } from '../store/productsSlice'
-import Greeting from './Greeting'
 import ProductsListItem from './ProductsListItem'
+import ArchiveSortPopover from './ArchiveSortPopover'
+import FlipMove from 'react-flip-move'
+import { sortProductsByNameAtoZ, sortProductsByNameZtoA } from '../store/compareFunctions'
 
 export default function ArchivedProductsList() {
   const archivedProducts = useSelector(selectors.archivedProducts)
   const isBlurred = useSelector((state) => state.drawer.isOpen)
+  const [isAtoZ, setIsAtoZ] = useState(true)
+  const history = useHistory()
 
-  return archivedProducts.length ? (
-    <UlStyled className={isBlurred ? 'blurred' : ''}>
-      {archivedProducts &&
-        archivedProducts.map(product => (
-          <ProductsListItem key={product.id} product={product} />
+  useEffect(() => {
+    if (!archivedProducts.length) {
+      history.push('/products')
+    }
+  }, [archivedProducts, history])
+
+  return (
+    <UlStyled className={isBlurred && 'blurred'}>
+      <ArchiveSortPopover isAtoZ={isAtoZ} setIsAtoZ={setIsAtoZ}/>
+      <FlipMove>
+        {archivedProducts &&
+        archivedProducts.sort(isAtoZ ? sortProductsByNameAtoZ : sortProductsByNameZtoA).map(product => (
+          <ProductsListItem key={product.id} product={product}/>
         ))}
+      </FlipMove>
     </UlStyled>
-  ) : (
-    <Greeting isBlurred={isBlurred} />
   )
 }
 
--- src/components/ArchivedProductsList.test.jsx
+++ src/components/ArchivedProductsList.test.jsx
@@ -8,24 +8,17 @@ describe('ArchivedProductsList.test.jsx', () => {
   const PRODUCTS_MOCK_DATA = {
     allProducts: [
       {
-        id: '001', name: 'test product', date: '2020-05-27', month: 6, size: '', price: ''
-      },{
+        id: '001', name: 'test product', date: '2020-05-27', month: 6, size: '', price: '',
+      }, {
         id: '002', name: 'test product 2', date: '2020-05-27', month: 6, size: '', price: '', isArchived: true,
       },
     ], selected: '001',
   }
 
-  it('shows the greeting when there are no products', () => {
-    const { getByTestId } = render(<ArchivedProductsList />)
-    const greeting = getByTestId('greeting')
-    expect(greeting).toBeInTheDocument()
-  })
-
   it('renders the archive product list when there are products with isArchived: true', () => {
-    render(<ArchivedProductsList />, {products: PRODUCTS_MOCK_DATA})
+    render(<ArchivedProductsList/>, { products: PRODUCTS_MOCK_DATA })
     expect(screen.queryByText(PRODUCTS_MOCK_DATA.allProducts[0].name)).not.toBeInTheDocument()
     expect(screen.getByText(PRODUCTS_MOCK_DATA.allProducts[1].name)).toBeInTheDocument()
   })
-
 })
 
